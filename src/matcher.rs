90898eb690df478510eb44290a37466e7bbfdb08
Roger Viñas <roger.vinas@n26.com>
2022-08-02 11:38:09 +0200

    Good implementation

--- src/matcher.rs
+++ src/matcher.rs
@@ -1,14 +1,24 @@
 pub fn matches(word: String, guess: String) -> String {
-    let mut result = String::from("");
-    for (i, word_char) in word.chars().enumerate() {
-        let guess_char = guess.chars().nth(i).unwrap();
-        if word_char == guess_char {
-            result.push('1');
-        } else {
-            result.push('0')
+    let mut word_mask: [char; 5] = ['0'; 5];
+    let mut guess_mask: [char; 5] = ['0'; 5];
+    for (i, guess_char) in guess.chars().enumerate() {
+        let word_char = word.chars().nth(i).unwrap();
+        if guess_char == word_char {
+            guess_mask[i] = '1';
+            word_mask[i] = '1';
         }
     }
-    return result;
+    for (i, guess_char) in guess.chars().enumerate() {
+        if guess_mask[i] == '0' {
+            for (j, word_char) in word.chars().enumerate() {
+                if word_mask[j] == '0' && guess_char == word_char {
+                    guess_mask[i] = '2';
+                    word_mask[j] = '2';
+                }
+            }
+        }
+    }
+    return guess_mask.iter().collect();
 }
 
 #[cfg(test)]
@@ -19,6 +29,7 @@ mod tests {
     #[rstest]
     #[case("JAZZY", "JAZZY", "11111")]
     #[case("JAZZY", "CRAZY", "00211")]
+    #[case("CRAZY", "CRAZC", "11110")]
     fn it_works(#[case] word: String, #[case] guess: String, #[case] output: String) {
         assert_eq!(matches(word, guess), output)
     }
